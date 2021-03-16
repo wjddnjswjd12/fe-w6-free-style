87939b2875d9e13d6bda68da576adbb70162dfcb
Jungwon Lee <leejungwon96@gmail.com>
2021-03-16 14:32:09 +0900

    [test] rendering img on canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,3 @@
-console.log("hi there");
-
 const body = {
   sidoName: "서울",
 };
@@ -11,3 +9,14 @@ fetch("http://localhost:3000/city", {
 })
   .then((res) => res.json())
   .then((result) => console.table(result.list[0]));
+
+window.onload = function () {
+  let canvasToday = document.getElementById("canvas_today");
+  let draw = canvasToday.getContext("2d");
+
+  let img = new Image();
+  img.src = "img/1.PNG";
+  img.onload = function () {
+    draw.drawImage(img, 0, 0, 600, 700);
+  };
+};
